2e7659249084f4a5f1e069bce86ae29be712f220
shyam <shyammistry96@gmail.com>
2022-11-19 15:44:13 +0000

    database updates

---
+++ src/Casino.Database/dbo/Tables/Game.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Game] (
+    [GameId]         UNIQUEIDENTIFIER NOT NULL,
+    [UserId]         UNIQUEIDENTIFIER NOT NULL,
+    [GameType]       VARCHAR (50)     NOT NULL,
+    [AmountStaked]   MONEY            NOT NULL,
+    [AmountWon]      MONEY            NOT NULL,
+    [WinProbability] DECIMAL (18)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([GameId] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
+);
+
---
+++ src/Casino.Database/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [Username]        VARCHAR (50)     NOT NULL,
+    [Balance]         DECIMAL (18)     NULL,
+    [LuckCoefficient] DECIMAL (1)      NULL,
+    [DateJoined]      DATETIME         NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
